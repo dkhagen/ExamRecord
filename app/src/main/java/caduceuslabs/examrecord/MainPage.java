be8987dd61e0f482f115d11dd30a4a1ccc3f0df7
David Hagen <dkhagen@Davids-MacBook-Pro-2.local>
2016-03-13 21:05:34 -0400

    second

---
+++ app/src/main/java/caduceuslabs/examrecord/MainPage.java
@@ -0,0 +1,67 @@
+package caduceuslabs.examrecord;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainPage extends AppCompatActivity {
+
+    private ListView todaysAppts;
+    private String[] testData = new String[] {"David Hagen", "John Smith"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+        todaysAppts = (ListView)findViewById(R.id.todaysAppts);
+        ArrayList<String> testList = new ArrayList<String>();
+        testList.add(testData[0]);
+        testList.add(testData[1]);
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.simple_list_item, testList);
+        todaysAppts.setAdapter(adapter); // TODO: create custom adapter from example online for formatting
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
