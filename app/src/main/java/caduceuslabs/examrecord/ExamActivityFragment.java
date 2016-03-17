680c404a2360e63b3badc1bc64b66bd27917f346
Alex BenDebba <alexandria.bendebba@gmail.com>
2016-03-17 02:20:57 -0400

    I hate android studios and no amount of money will make this worth it

--- app/src/main/java/caduceuslabs/examrecord/ExamActivityFragment.java
+++ app/src/main/java/caduceuslabs/examrecord/ExamActivityFragment.java
@@ -17,6 +17,6 @@ public class ExamActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_exam_basic_info, container, false);
+        return inflater.inflate(R.layout.fragment_exam_history, container, false);
     }
 }
